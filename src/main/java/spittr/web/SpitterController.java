6c4650b7fee4b9fce0987a7a398777bf8bb0b1c1
Jlobici Alexandr <sunrise.battle@gmail.com>
2017-10-23 23:16:14 +0300

    Changed pom.xml: added scope provided for javax.el-api

--- src/main/java/spittr/web/SpitterController.java
+++ src/main/java/spittr/web/SpitterController.java
@@ -3,6 +3,7 @@ package spittr.web;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
